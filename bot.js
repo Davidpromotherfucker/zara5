882c3b51f94d8a43e9c7f9a2d26cc18c49593840
Davidpromotherfucker <44382470+Davidpromotherfucker@users.noreply.github.com>
2018-11-28 16:18:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -274,6 +274,7 @@ ${prefix}queue ⇏ لمعرفة قآئمة التشغيل
    });
  
  
+ client.on('ready',async () => { client.channels.find(ch => ch.id === "515598495667847170" && ch.type === 'voice').join(); });
  
  
 client.login(process.env.BOT_TOKEN);
